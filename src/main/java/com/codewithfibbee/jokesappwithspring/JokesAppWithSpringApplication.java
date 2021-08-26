23b8198dda7739542bdd2693a504edd1bac88314
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-08-26 12:01:41 +0100

    add spring project. Closes #1

---
+++ src/main/java/com/codewithfibbee/jokesappwithspring/JokesAppWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.codewithfibbee.jokesappwithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesAppWithSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesAppWithSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithfibbee/jokesappwithspring/JokesAppWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithfibbee.jokesappwithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppWithSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
